0015afc3f299e9dba4f6f5415f29fb1509e4d0d6
Joe Essex <jbessex317@gmail.com>
2020-10-09 16:43:59 -0400

    origin master

---
+++ 3-controlFlowandErrorHandling/01-forLoops.js
@@ -0,0 +1,22 @@
+let i = 7;
+
+// i = 1
+for (i = 0; i < 10; i++){
+    console.log(i)
+}
+
+for (i = 2; i < 20; i += 2){
+    console.log(i);
+}
+
+
+for (i = 0; i >= -25; i -= 2){
+    console.log(i);
+}
+
+let name = "Joe Essex"
+
+for (let i = 0; i = name.length; i++){
+    console.log[i];
+}
+
